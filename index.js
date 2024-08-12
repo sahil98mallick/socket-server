d47437bbeba952a71a7a1ff8d1c6f37f0f3e648e
sahil98mallick <sahil98mallick@gmail.com>
2024-08-12 21:55:52 +0530

    changes

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socket-client-five.vercel.app",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
